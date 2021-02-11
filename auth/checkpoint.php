f83e6c235d227e67daf1421f5efcec44153034f9
ahmedsay101 <75319864+ahmedsay101@users.noreply.github.com>
2021-02-11 04:13:15 +0200

    Update checkpoint.php

--- auth/checkpoint.php
+++ auth/checkpoint.php
@@ -1,7 +1,7 @@
 <?php
 require_once("config.php");
 require_once("functions.php");
-require_once("FormSanitizer.php");
+
 if(!isset($_SESSION["userLoggedIn"]) || !isset($_SESSION["confirmationCode"])) {
     exit();
 }
